88e16f7d578702faea02d005c20169d9c8b6dd70
feici02 <feici02@163.com>
2017-05-13 16:23:10 +0800

    add python

---
+++ bash/variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# default value
+MYNAME=${NAME:-feici02}
+echo ${MYNAME}
+
+NAME=mickey
+MYNAME=${NAME:-feici02}
+echo ${MYNAME}
+
---
+++ python/hello.py
@@ -0,0 +1,13 @@
+def world():
+    print("Hello, world!")
+
+name = "Jason"
+
+class Octopus:
+    def __init__(self, name, color):
+        self.color = color
+        self.name = name
+
+    def self_introduce(self):
+        print("This octopus is " + self.color + ".")
+        print(self.name + " is the octopus's name.")
